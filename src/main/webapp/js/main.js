2dc49dfd34d5188ba974f474161f235c1f1d4a2f
Rennan Prysthon <rennandelcastillo@gmail.com>
2024-12-08 22:55:28 -0300

    refact: Removendo classes de front

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,18 +1,2 @@
 
-class Votacao {
-  constructor() {
-
-  }
-
-  montarTeclado() {
-    const mainElement = document.querySelector("main")
-  }
-
-  iniciar () {
-    console.log("iniciando")
-  }
-}
-
-let votacao = new Votacao();
-votacao.iniciar();
 
